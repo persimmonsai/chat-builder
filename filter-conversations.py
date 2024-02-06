0cd5a827bef0750c73b6d6532ca602b1ddf3aa32
Don Mahurin <2797413+dmahurin@users.noreply.github.com>
2024-02-06 15:29:18 -0800

    filter-conversations: change to test exclude words against words in any line

--- filter-conversations.py
+++ filter-conversations.py
@@ -2,6 +2,7 @@
 
 import sys
 import json
+import re
 
 if len(sys.argv) < 2:
    print(f"usage: {sys.argv[0]} CONFIG.JSON", file=sys.stderr)
@@ -10,12 +11,13 @@ if len(sys.argv) < 2:
 with open(sys.argv[1]) as f:
    config = json.load(f)
 
+exclude_set = set(config['exclude']) if config['exclude'] is not None else None
+
 curr_convo = []
 skip_convo = False
 
 for _line in sys.stdin:
    line = _line.rstrip()
-   line_words = line.split(" ")
 
    if line == "": # signals end of convo
       if not skip_convo and len(curr_convo) != 0:
@@ -24,7 +26,11 @@ for _line in sys.stdin:
       curr_convo = []
    elif not skip_convo:
       curr_convo.append(line)
-      if line_words[0] == "TERM:":
-         term = " ".join(line_words[1:])
-         if term not in config['words'] or term in config['exclude']:
+      if line.startswith("TERM: "):
+         term = line[line.index(" ")+1:]
+         if term not in config['words']:
+            skip_convo = True
+      else:
+         line_words = re.split(r'[^a-zA-Z0-9]+', line)
+         if exclude_set.intersection(line_words):
             skip_convo = True
